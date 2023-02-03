837f39b61fe298ca9c88bba737bb09f86e2af345
notsumez <adahorapirola@gmail.com>
2023-02-03 17:21:26 -0300

    menu adm arrumado

--- admin/menu_adm.php
+++ admin/menu_adm.php
@@ -5,12 +5,11 @@
     <title>Área Administrativa</title>
     <meta charset="UTF-8">
     <!-- Link arquivos Bootstrap CSS -->
-    <!-- CÓDIGO DESABILITADO PARA NÃO HAVER CONFLITOS 
+ 
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="../css/bootstrap.min.css" rel="stylesheet">
-    -->
-    <!-- Link para CSS específico -->
+
     
 </head>
 <body>
@@ -61,9 +60,9 @@
 
     
 <!-- Link arquivos Bootstrap js -->
-<!-- CÓDIGO DESABILITADO PARA NÃO HAVER CONFLITOS 
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
--->
+
 </body>
 </html>
