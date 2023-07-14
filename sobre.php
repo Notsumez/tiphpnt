0fe4bfaecb31aedec7cbd558a94638d968e1a03f
Andrey <adahorapirola@gmail.com>
2023-07-14 01:39:56 -0300

    MVV e começo do trabalhe conosco

--- sobre.php
+++ sobre.php
@@ -7,7 +7,75 @@
     <link rel="stylesheet" href="css/estilo.css">
     <title>Sobre a Braners Carners</title>
 </head>
-<body>
-    
+<body style="background-color: #2b2b2b;">
+    <?php include 'menu_publico.php';?>
+    <section class="container container-fluid">
+        <div class="text-center" style="color: white;">
+            <h2><b>O que é a Braners Carners?</b></h2> 
+            <br>
+            <div style="display: flex;">
+                <div>
+                    <img src="images/giga_chad.jpg" width="450px" alt="Fundador da Braners Carners (Ficitício)" style="border-radius: 20px;"> 
+                </div>
+                <div style="margin: 15px;">
+                    <p>
+                        Surgida em 2021 como projeto integrador do curso de Técnico em Informática do Senac Itaquera, 
+                        a Braners Carners é uma aplicação web desenvolvida para uma Churrascaria Fictícia com o 
+                        intuíto de aprimorar e testar as habilidades dos alunos individualmente. 
+                        Segue uma história fictícia sobre a casa de carnes:
+                    </p>  
+                    <p>A Braners Carners é uma churrascaria que ganhou vida em 2021, como um projeto integrador do curso de Técnico em Informática do Senac Itaquera. Seu dono, Sr. Antônio (homem na foto ao lado), sempre foi apaixonado por carnes e decidiu realizar seu sonho de ter um negócio próprio.
+                        Desde jovem, Antônio acompanhava seu pai nas festas de família, onde o churrasco era o protagonista. Ele aprendeu a arte de preparar carnes suculentas, dominando técnicas de tempero e churrasqueira. Essa paixão pelas brasas e sabores se tornou sua marca registrada.
+                        Com a experiência adquirida ao longo dos anos e o desejo de proporcionar uma experiência única aos amantes de carne, Antônio decidiu abrir a Braners Carners. Ele queria criar um ambiente acolhedor, onde as pessoas pudessem desfrutar de carnes de alta qualidade em um ambiente agradável e descontraído.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br>
+    <hr width="88%">
+    <br>
+    <section>
+        <div>
+            <div style="display: flex; justify-content: center; margin-right: 55px; margin-left: 55px;">
+                <div class="text-center" style="margin-right: 20px;">
+                    <img src="images/missao.svg" width="80px" alt="">
+                    <h2 style="color: white;">MISSÃO</h2>
+                    <p style="color: white;">Entregar o melhor atendimento e alimento sobre carnes para o <br> 
+                    seu churrasco.</p>
+                </div>
+                <div class="text-center" style="margin-right: 20px;">
+                    <img src="images/visao.svg" width="80px" alt="">
+                    <h2 style="color: white;">VISÃO</h2>
+                    <p style="color: white;">Transformar a sua experiência o melhor possível, de modo que <br> 
+                        mude o seu ver de um churrasco.</p>
+                </div>
+                <div class="text-center">
+                    <img src="images/valores.svg" width="80px" alt="">
+                    <h2 style="color: white;">VALORES</h2>
+                    <p style="color: white;">Responsabilidade profissional, qualidade e rapidez.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br>
+    <hr width="88%">
+    <br>
+    <section>
+        <div class="text-center" style="color: white;">
+            <h2><b>Trabalhe Conosco</b></h2> 
+            <br>
+            <div style="display: flex; justify-content: center; margin-left: 50px; margin-right: 50px;">
+                <div>
+                    <p>Quer fazer parte do delicioso mundo do churrasco, entretendo e servindo os clientes da melhor forma possível? Deixe seus dados e faça parte da nossa equipe!</p>
+                    <p>Você também receberá um treinamento de nosso mestre churrasqueiro para compreender as melhores técnicas de como fazer um churrasco de alta qualidade.</p>
+                    <button style="color: #E6B53B; background-color: #AF2319;; border: 1px solid #E6B53B;">Saiba mais...</button>
+                </div>
+                <div>
+                    <img src="images/funcionaro.jpg" style="border-radius: 50px;" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
