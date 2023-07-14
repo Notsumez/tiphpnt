279b8e5b5ce7d0886bffbe68b2451ad778dcffdb
Andrey <adahorapirola@gmail.com>
2023-07-14 12:07:37 -0300

    ajustes no botão e fonte do sobre

--- sobre.php
+++ sobre.php
@@ -17,7 +17,7 @@
                 <div>
                     <img src="images/giga_chad.jpg" width="450px" alt="Fundador da Braners Carners (Ficitício)" style="border-radius: 20px;"> 
                 </div>
-                <div style="margin: 15px;">
+                <div style="margin: 15px;" class="txtSobre">
                     <p>
                         Surgida em 2021 como projeto integrador do curso de Técnico em Informática do Senac Itaquera, 
                         a Braners Carners é uma aplicação web desenvolvida para uma Churrascaria Fictícia com o 
@@ -38,19 +38,19 @@
     <section>
         <div>
             <div style="display: flex; justify-content: center; margin-right: 55px; margin-left: 55px;">
-                <div class="text-center" style="margin-right: 20px;">
+                <div class="text-center txtSobre" style="margin-right: 20px;">
                     <img src="images/missao.svg" width="80px" alt="">
                     <h2 style="color: white;">MISSÃO</h2>
                     <p style="color: white;">Entregar o melhor atendimento e alimento sobre carnes para o <br> 
                     seu churrasco.</p>
                 </div>
-                <div class="text-center" style="margin-right: 20px;">
+                <div class="text-center txtSobre" style="margin-right: 20px;">
                     <img src="images/visao.svg" width="80px" alt="">
                     <h2 style="color: white;">VISÃO</h2>
                     <p style="color: white;">Transformar a sua experiência o melhor possível, de modo que <br> 
                         mude o seu ver de um churrasco.</p>
                 </div>
-                <div class="text-center">
+                <div class="text-center txtSobre">
                     <img src="images/valores.svg" width="80px" alt="">
                     <h2 style="color: white;">VALORES</h2>
                     <p style="color: white;">Responsabilidade profissional, qualidade e rapidez.</p>
@@ -66,13 +66,13 @@
             <h2><b>Trabalhe Conosco</b></h2> 
             <br>
             <div style="display: flex; justify-content: center; margin-left: 50px; margin-right: 50px;">
-                <div>
+                <div class="txtSobre" style="margin-right: 20px;">
                     <p>Quer fazer parte do delicioso mundo do churrasco, entretendo e servindo os clientes da melhor forma possível? Deixe seus dados e faça parte da nossa equipe!</p>
                     <p>Você também receberá um treinamento de nosso mestre churrasqueiro para compreender as melhores técnicas de como fazer um churrasco de alta qualidade.</p>
-                    <button style="color: #E6B53B; background-color: #AF2319;; border: 1px solid #E6B53B;">Saiba mais...</button>
+                    <button>Saiba mais...</button>
                 </div>
                 <div>
-                    <img src="images/funcionaro.jpg" style="border-radius: 50px;" alt="">
+                    <img src="images/funcionaro.jpg" style="border-radius: 20px;" alt="">
                 </div>
             </div>
         </div>
